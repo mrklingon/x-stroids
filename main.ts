3256ddeb384bc9b2f1c89fb273653a960fc9bec0
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-08-19 15:02:48 -0500

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,30 @@
+namespace SpriteKind {
+    export const rock = SpriteKind.create()
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     fire = 40
+})
+sprites.onOverlap(SpriteKind.rock, SpriteKind.rock, function (sprite, otherSprite) {
+    sprite.destroy()
+    newAsteroid()
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    lbolt = sprites.create(laser[direction], SpriteKind.Projectile)
+    lbolt.setPosition(XWing.x, XWing.y)
+    lbolt.setVelocity(xvel[direction] * 10, yvel[direction] * 10)
+    lbolt.setFlag(SpriteFlag.DestroyOnWall, true)
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     direction = direction - 1
     if (direction < 0) {
         direction = direction + 8
     }
+})
+sprites.onOverlap(SpriteKind.rock, SpriteKind.Player, function (sprite, otherSprite) {
+    sprite.destroy()
+    newAsteroid()
+    info.changeLifeBy(-1)
+    scene.cameraShake(4, 500)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     direction = direction + 1
@@ -16,9 +35,28 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     fire = 0
 })
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.rock, function (sprite, otherSprite) {
+    info.changeScoreBy(5)
+    otherSprite.destroy()
+    newAsteroid()
+})
+function newAsteroid () {
+    asteroid = sprites.create(Asts[randint(0, 3)], SpriteKind.rock)
+    asteroid.setPosition(randint(0, 160), randint(0, 120))
+    asteroid.setVelocity(randint(0, 65), randint(0, 65))
+    asteroid.setBounceOnWall(true)
+}
+let lbolt: Sprite = null
+let yvel: number[] = []
+let xvel: number[] = []
 let direction = 0
+let XWing: Sprite = null
+let laser: Image[] = []
+let asteroid: Sprite = null
+let Asts: Image[] = []
 let fire = 0
 fire = 0
+game.splash("Pilot your X-Wing through the asteroids")
 let Xwing = [
 assets.image`myImage`,
 assets.image`myImage4`,
@@ -29,10 +67,101 @@ assets.image`myImage6`,
 assets.image`myImage2`,
 assets.image`myImage3`
 ]
-let XWing = sprites.create(Xwing[0], SpriteKind.Player)
+Asts = [
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . f f f . . . . 
+    . . . . . . f f f f c f . . . . 
+    . . . . . f f c c c c f f f f . 
+    . . . . . f c c c c c c c c f . 
+    . . . f f f c c c c c c c c f . 
+    . . . f c c c c c c c c c c f . 
+    . . . f f c c c f f c c c c f . 
+    . . . . f f c c f f f c c c f f 
+    . . . . . f f c f . f f c c c f 
+    . . . . . . f f f . . f c f f f 
+    . . . . . . . . . . . f f f . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . c c . . . . . . . 
+    . . . . . . c c 1 c . . . . . . 
+    . . . c c c c 1 1 1 c c . . . . 
+    . . c c 1 1 1 1 1 1 1 5 c . . . 
+    . . c c 1 1 1 1 1 1 5 5 c c . . 
+    . . c 1 1 1 1 1 1 1 5 1 1 c . . 
+    . . c c 5 5 5 5 5 1 1 1 1 c . . 
+    . . . c 1 1 5 5 1 1 1 1 c . . . 
+    . . . c c 1 1 1 c c c c . . . . 
+    . . . . c c 1 c c 7 7 7 c c . . 
+    . . . . . c 1 1 1 5 7 7 7 c c . 
+    . . . . . c c 1 1 1 5 7 c . c . 
+    . . . . . . c c 1 1 5 5 1 c . . 
+    . . . . . . . . c c 1 5 c c . . 
+    . . . . . . . . . . c c . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . e e e e e e e . . . 
+    . . . . . e e b b b b b e e e . 
+    . . . e e e b b b b b b b b b e 
+    . . . e b b b b b b b b b f f e 
+    . . e e b b b b b b b b f b b e 
+    . . e b b b b f f b b b b b b e 
+    . . e b b b b b f f f b b b b e 
+    . . e b b b b b b b f b b b b e 
+    . . e e b b b b b b f f b b b e 
+    . . . e e b b b b b b f f b b e 
+    . . . . e e b b b b b b e e e e 
+    . . . . . e e e e e e e e . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . d d d . . . . . . . 
+    . . . d d d d f f d d . . . . . 
+    . . d f f f f f f f d d d . . . 
+    . . d d f f f f f f f f d d . . 
+    . . d d f f f f f f f f d d d . 
+    . d f f f f f f f f f f f f d . 
+    . d f f f f f f f f f f f f d . 
+    . d d f f f f f f d f f f f d . 
+    . . d d d f f f f d f f f d . . 
+    . . . . . d d d d d f f f d . . 
+    . . . . . d d d . . d d f d . . 
+    . . . . . . . . . . d d d . . . 
+    . . . . . . . . . . . . d . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `
+]
+for (let index = 0; index <= 3; index++) {
+    asteroid = sprites.create(Asts[randint(0, 3)], SpriteKind.rock)
+    asteroid.setPosition(randint(0, 160), randint(0, 120))
+    asteroid.setVelocity(randint(0, 65), randint(0, 65))
+    asteroid.setBounceOnWall(true)
+}
+info.setLife(10)
+laser = [
+assets.image`l1`,
+assets.image`myImage8`,
+assets.image`l2`,
+assets.image`myImage7`,
+assets.image`l1`,
+assets.image`myImage8`,
+assets.image`l2`,
+assets.image`myImage7`
+]
+XWing = sprites.create(Xwing[0], SpriteKind.Player)
 XWing.setStayInScreen(true)
 direction = 0
-let xvel = [
+xvel = [
 50,
 25,
 0,
@@ -44,7 +173,7 @@ let xvel = [
 ]
 scene.setBackgroundColor(8)
 effects.starField.startScreenEffect()
-let yvel = [
+yvel = [
 0,
 25,
 50,
@@ -68,8 +197,8 @@ game.onUpdate(function () {
     XWing.setImage(Xwing[direction])
     if (fire > 0) {
         XWing.setVelocity(xvel[direction], yvel[direction])
-        fire += -1
     } else {
+        fire += -1
         XWing.setVelocity(0, 0)
     }
 })
